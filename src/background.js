18bcc6bf04273f5fef2256ea8376c4a7bc22cf67
Bluscream <Bluscream@users.noreply.github.com>
2024-06-30 20:32:53 +0200

    v2ify

--- src/background.js
+++ src/background.js
@@ -1,25 +1,37 @@
-chrome.action.onClicked.addListener((tab) => {
-    // Attempt to reload the tab
-    chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    files: ['inject.js']
-    });
+chrome.browserAction.onClicked.addListener(function(tab) {
+    chrome.tabs.executeScript(null, {file: 'inject.js'});
   });
   
-  function checkTabStatus(tabId) {
-    chrome.tabs.get(tabId, async (tab) => {
+  function checkTabStatus(queryInfo, callback) {
+    chrome.tabs.query(queryInfo, function(tabs) {
+      if (tabs.length > 0) {
+        const tabId = tabs[0].id;
+        checkTabStatusInternal(tabId, callback);
+      } else {
+        console.log("No tabs found.");
+      }
+    });
+  }
+  
+  function checkTabStatusInternal(tabId, callback) {
+    chrome.tabs.get(tabId, function(tab) {
       if (tab.status === 'crashed') {
         // Tab has crashed, attempt to reload
-        await chrome.tabs.reload(tabId);
+        chrome.tabs.reload(tabId);
+        if (callback) {
+          callback(); // Call the callback after reloading the tab
+        }
       } else {
         // Schedule next check if tab is not crashed
-        setTimeout(() => {
-          checkTabStatus(tabId);
+        setTimeout(function() {
+          checkTabStatusInternal(tabId, callback);
         }, 5000); // Check every 5 seconds
       }
     });
   }
   
   // Start checking status of monitored tabs
-  checkTabStatus(chrome.tabs.query({}).then(tabs => tabs[0].id));
+  checkTabStatus({}, function() {
+    console.log("Checked all tabs.");
+  });
   
